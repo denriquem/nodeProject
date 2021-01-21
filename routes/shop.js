acb04c4d3ce11f646b167cfada505d80b8e2a4f6
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-01-21 19:59:48 +0100

    pug templating setup

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const rootDir = require("../helpers/path");
+const adminData = require("./admin");
 
 const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = router;
