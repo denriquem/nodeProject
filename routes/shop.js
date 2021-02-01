efd9c72a40187ef0a6edd1d7671937bfc7eb70c4
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-02-01 20:15:22 +0100

    finished pug template

--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,8 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+	const products = adminData.products;
+	res.render("shop", { prods: products, pageTitle: "Shop", path: "/shop" });
 });
 
 module.exports = router;
