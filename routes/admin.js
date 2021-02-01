eca558c1f452d67f9b4e05f5b5149d6199af016d
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-02-01 19:55:58 +0100

    adding a pug layout

--- routes/admin.js
+++ routes/admin.js
@@ -7,13 +7,14 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+	res.sendFile(path.join(rootDir, "views", "add-product.html"));
+	res.render("add-product", { pageTitle: "Add Product" });
 });
 
 router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  console.log(req.body);
-  res.redirect("/");
+	products.push({ title: req.body.title });
+	console.log(req.body);
+	res.redirect("/");
 });
 
 exports.routes = router;
